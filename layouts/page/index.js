ec1bbf4a5c17366b9117563078f6b171bcb514ff
Yun <wushujia@vip.qq.com>
2018-12-10 16:14:06 +0800

    添加页面布局

---
+++ layouts/page/index.js
@@ -0,0 +1,13 @@
+/**
+ * 页面布局
+ * 
+ * 用法：
+ * 
+ * import PageLayout from '@vue-common/layouts/page'
+ * Vue.use(PageLayout)
+ */
+import PageLayout from './index.vue'
+
+export default (Vue) => {
+  Vue.component('page-layout', PageLayout)
+}
---
+++ layouts/page/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLayout'
+}
+</script>
+
+<style lang="less" scoped>
+.page {
+  padding: 20px;
+  background: #FFF;
+}
+</style>
