2eb6c2d0ddffbca99c44e5e326c3dd6025985808
Yun <wushujia@vip.qq.com>
2019-02-20 16:34:30 +0800

    添加time过滤器

--- filters/index.js
+++ filters/index.js
@@ -3,10 +3,12 @@ import constant from './constant'
 import number from './number'
 import trim from './trim'
 import bankcard from './bankcard'
+import time from './time'
 export default {
   datetime,
   constant,
   number,
   trim,
-  bankcard
+  bankcard,
+  time
 }
