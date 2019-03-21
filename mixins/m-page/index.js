bccbde6496c59f38ba4307d98970ab2bc2c99277
Yun <wushujia@vip.qq.com>
2019-03-21 10:35:03 +0800

    移动端分页

--- mixins/m-page/index.js
+++ mixins/m-page/index.js
@@ -47,51 +47,6 @@ export default {
      */
     pageChange (page) {
       this.getList(page)
-    },
-    /**
-     * ===========================================================
-     * 页面容量发生改变
-     * ===========================================================
-     */
-    pageSizeChange (pageSize) {
-      // 新的页面容量 >= 旧的页面容量
-      if (this.page.current === 1) {
-        // 当前处在第一页
-        if (pageSize < this.page.size) {
-          // 新的页面容量 < 旧的页面容量
-          this.page.size = pageSize
-          this.table.data.splice(
-            0,
-            this.table.data.length,
-            ...this.table.data.splice(
-              0,
-              Math.min(this.page.size, this.page.total)
-            )
-          )
-          // this.getList(this.page.current)
-          return
-        }
-        if (this.page.total > this.page.size) {
-          // 总数据量 > 旧的页面容量
-          this.page.size = pageSize
-          this.getList(1)
-        } else {
-          // 总数据量 <= 旧的页面容量
-          this.page.size = pageSize
-        }
-      } else {
-        // 不是处在第一页，如果处在当前页是否会有数据
-        if (pageSize * (this.page.current - 1) > this.page.total) {
-          // 当前页的前一页的数据量 > 总数据量
-          // 计算最后一页
-          let targetPage = parseInt(this.page.total / pageSize) + 1
-          this.page.size = pageSize
-          this.getList(targetPage)
-        } else {
-          this.page.size = pageSize
-          this.getList(this.page.current)
-        }
-      }
     }
   }
 }
