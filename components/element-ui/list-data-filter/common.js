715dfc3ef882bf79a4fcb028ebca3d0a65587b30
Yun <wushujia@vip.qq.com>
2019-08-30 16:29:26 +0800

    检索修复

--- components/element-ui/list-data-filter/common.js
+++ components/element-ui/list-data-filter/common.js
@@ -13,7 +13,7 @@ export default {
       if (obj.hasOwnProperty(key)) {
         switch (obj[key].type) {
           default:
-            filter[key] = obj[key].value ? obj[key].value : undefined
+            filter[key] = (obj[key].value || obj[key].value === 0) ? obj[key].value : undefined
         }
       }
     }
